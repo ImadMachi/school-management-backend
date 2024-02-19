3b0d1458abc95660b480fdff84dcfd2ce8d58c0c
AchrafKhGit <kharbabachraf@gmail.com>
2024-02-19 12:02:06 +0100

    backend

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,56 +1,28 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 import { User } from '../users/entities/user.entity';
 import { Student } from '../students/entities/student.entity';
 import { Teacher } from '../teachers/entities/teacher.entity';
 import { Administrator } from '../administrators/entities/administrator.entity';
-import { Parent } from '../parents/entities/parent.entity';
+import { Parent } from 'src/parents/entities/parent.entity';
 import { Role } from '../roles/entities/role.entity';
 import { Message } from '../messages/entities/message.entity';
 import { Attachment } from '../messages/entities/attachment.entity';
 import { Director } from '../director/entities/director.entity';
 import { MessageCategory } from '../message-categories/entities/message-category.entity';
-const { parse } = require('pg-connection-string');
 
 dotenv.config();
 
-let type = process.env.DB_TYPE || 'postgres';
-let host = process.env.DB_HOST;
-let port = +process.env.DB_PORT;
-let username = process.env.DB_USER;
-let password = process.env.DB_PASS;
-let database = process.env.DB_NAME;
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (databaseUrl) {
-  const dbConfig = parse(databaseUrl);
-  host = dbConfig.host;
-  port = +dbConfig.port;
-  username = dbConfig.user;
-  password = dbConfig.password;
-  database = dbConfig.database;
-}
-
 export const dataSourceOptions: DataSourceOptions = {
-  type: type as any,
-  host,
-  port,
-  username,
-  password,
-  database,
-  entities: [User, Role, Student, Teacher, Administrator, Parent, Message, Attachment, Director, MessageCategory],
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [User, Role, Student, Teacher, Administrator,Parent, Message, Attachment, Director, MessageCategory],
   synchronize: process.env.NODE_ENV == 'development',
   // @ts-ignore
   autoLoadEntities: true,
-  logging: process.env.NODE_ENV == 'development',
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-  sslmode: 'require',
 };
-
-export default new DataSource(dataSourceOptions);
