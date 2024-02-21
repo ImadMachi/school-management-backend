eb6f619c02bcae659033574c4eef22b6c08f30ed
AchrafKhGit <kharbabachraf@gmail.com>
2024-02-21 13:08:09 +0100

    new role director

--- src/director/director.controller.ts
+++ src/director/director.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Delete, Patch, Param } from '@nestjs/common';
 import { DirectorService } from './director.service';
 import { CreateDirectorDto } from './dto/create-director.dto';
 import { CheckPolicies } from 'src/casl/guards/policies.guard';
 import { ManageDirectorsPolicyHandler } from 'src/casl/policies/directors/manage-directors.policy';
+import { UpdateDirectorDto } from './dto/update-director.dto';
 
 @Controller('directors')
 export class DirectorController {
@@ -20,18 +21,20 @@ export class DirectorController {
     return this.directorService.findAll();
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.directorService.findOne(+id);
-  // }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.directorService.findOne(+id);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateDirectorDto: UpdateDirectorDto) {
-  //   return this.directorService.update(+id, updateDirectorDto);
-  // }
+  @Patch(':id')
+  @CheckPolicies(new ManageDirectorsPolicyHandler())
+  update(@Param('id') id: string, @Body() updateDirectorDto: UpdateDirectorDto) {
+    return this.directorService.update(+id, updateDirectorDto);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.directorService.remove(+id);
-  // }
+  @Delete(':id')
+  @CheckPolicies(new ManageDirectorsPolicyHandler())
+  remove(@Param('id') id: string) {
+    return this.directorService.remove(+id);
+  }
 }
--- src/director/director.service.ts
+++ src/director/director.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDirectorDto } from './dto/create-director.dto';
 import { UpdateDirectorDto } from './dto/update-director.dto';
 import { Director } from './entities/director.entity';
@@ -42,16 +42,42 @@ export class DirectorService {
   findAll() {
     return this.directorRepository.find();
   }
+  findOne(id: number) {
+    return this.directorRepository.findOne({
+      where: { id },
+    });
+  }
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} director`;
-  // }
+  async update(id: number, updateDirectorDto: UpdateDirectorDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    let director: Director;
+    try {
+      director = await this.directorRepository.findOne({ where: { id } });
+      if (!director) {
+        throw new NotFoundException();
+      }
 
-  // update(id: number, updateDirectorDto: UpdateDirectorDto) {
-  //   return `This action updates a #${id} director`;
-  // }
+      this.directorRepository.merge(director, updateDirectorDto);
+      await this.directorRepository.save(director);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
+      throw new HttpException(error.message, error.status);
+    }
+    await queryRunner.release();
+    return director;
+  }
+  async remove(id: number) {
+    const director = await this.directorRepository.findOne({
+      where: { id },
+    });
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} director`;
-  // }
+    if (!director) {
+      throw new NotFoundException();
+    }
+    return this.directorRepository.delete(id);
+  }
+
 }
