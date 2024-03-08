8da227729bd18f26c3058c806d6a74e426740aca
AchrafKhGit <kharbabachraf@gmail.com>
2024-03-08 01:34:41 +0100

    new modification for profile image

---
+++ src/database/dummyUserImages.ts
@@ -0,0 +1,15 @@
+export const menProfileImages = [
+    '/profile-images/men/user.jpeg',
+    '/profile-images/men/user2.jpeg',
+    '/profile-images/men/user3.jpeg',
+    '/profile-images/men/user4.jpeg',
+
+  ];
+  
+export const womenProfileImages = [
+    '/profile-images/women/user.jpeg',
+    '/profile-images/women/user2.jpeg',
+    '/profile-images/women/user3.jpeg',
+    '/profile-images/women/user4.jpeg',
+  ];
+  
